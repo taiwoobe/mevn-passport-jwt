da339f81ac62d93abddfedd6faee29bcfccce722
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-10-22 15:59:23 +0100

    installing the vuex persistedstate plugin for page reloads

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import { HTTP } from '../api/api'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     token: localStorage.getItem('token') || '',
     status: '',
